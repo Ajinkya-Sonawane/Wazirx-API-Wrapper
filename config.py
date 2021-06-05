f1ca6c34e6d5806a96b42f15bfa1e527210a8468
Ajinkya-Sonawane <sonawaneajinks@gmail.com>
2021-06-05 14:13:10 +0530

    Add config.py - WazirX API endpoints

---
+++ config.py
@@ -0,0 +1,4 @@
+# Following URLs were taken from: https://github.com/WazirX/wazirx-api 
+URL_MARKET_STATUS = 'https://api.wazirx.com/api/v2/market-status'
+URL_TICKERS = 'https://api.wazirx.com/api/v2/tickers'
+URL_MARKET_DEPTH = 'https://api.wazirx.com/api/v2/depth?market='
